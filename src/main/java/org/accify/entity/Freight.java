9723c3f8185d9c57bc4e6a5ecc2ca62a8d333736
neer09 <iamworldchanger25@gmail.com>
2025-08-24 14:43:07 +0530

    initial commit

---
+++ src/main/java/org/accify/entity/Freight.java
@@ -0,0 +1,30 @@
+package org.accify.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "freight")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Freight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+    private String consignor;
+    private String consignee;
+    private String fromLocation;
+    private String toLocation;
+    private String vehicleNo;
+    private Double weight;
+    private Double amount;
+    private Double gst;
+    private Double total;
+}
