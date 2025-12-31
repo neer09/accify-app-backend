f48f1890a599ea6ba367472f8169a34f437e53f4
neer09 <iamworldchanger25@gmail.com>
2026-01-01 01:18:08 +0530

    scheduler

--- src/main/java/org/accify/scheduler/MonthlySellLogCleanupScheduler.java
+++ src/main/java/org/accify/scheduler/MonthlySellLogCleanupScheduler.java
@@ -1,6 +1,8 @@
 package org.accify.scheduler;
 
 import org.accify.repo.ETFSellNotificationLogRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,15 +10,17 @@ import org.springframework.stereotype.Component;
 public class MonthlySellLogCleanupScheduler {
 
     private final ETFSellNotificationLogRepository logRepository;
+    private static final Logger log = LoggerFactory.getLogger(MonthlySellLogCleanupScheduler.class);
 
     public MonthlySellLogCleanupScheduler(ETFSellNotificationLogRepository logRepository) {
         this.logRepository = logRepository;
     }
 
-    // Runs at 12:10 AM on the 1st day of every month
-    @Scheduled(cron = "0 0 1 1 * ?")
+    // Runs at 1:30 AM on the 1st day of every month
+    @Scheduled(cron = "0 30 1 1 * ?")
     public void cleanupLogs() {
+        log.info("Deleting all ETF sell logs on the 1st of the month.");
         logRepository.deleteAllLogs();
-        //System.out.println("Deleted all ETF sell logs on the 1st of the month.");
+        log.info("Deleted all ETF sell logs on the 1st of the month.");
     }
 }
